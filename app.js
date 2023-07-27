c3f6ca05d82fc6884bd273df4121c9c29a45cdf6
sato-ayumi <129816397+sato-ayumi@users.noreply.github.com>
2023-07-27 13:11:43 +0000

    [Finish]javascript2

--- app.js
+++ app.js
@@ -29,11 +29,51 @@
 
 // alert('1から10まで足し算した結果は' + num + 'です');
 
-let alertString;
-alertString = addString("WebCamp");
-alert(alertString);
+// let alertString;
+// alertString = addString("WebCamp");
+// alert(alertString);
 
-function addString(strA){
-  let addStr = "Hello" + strA;
-  return addStr;
+//関数について
+// function addString(strA){
+//   let addStr = "Hello" + strA;
+//   return addStr;
+// }
+
+// let promptStr = prompt('何か好きな文字を入力してください。');
+// alert(promptStr);
+
+// let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+
+// alert('あなたの選んだ手は' + user_hand + 'です。');
+
+let user_hand = prompt('ジャンケンの手をグー、チョキ、パーから選んでください。');
+while ((user_hand != "グー") && (user_hand != "チョキ") && (user_hand != "パー") && (user_hand != null)){
+  alert ('グー、チョキ、パーのいずれかを入力してください');
+  user_hand = prompt('ジャンケンの手をグー、チョキ、パーから選んでください。');
+}
+
+let js_hand = getJShand();
+let judge = winLose(user_hand, js_hand);
+
+if (user_hand != null){
+  alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+} else {
+  alert("またチャレンジしてね")
+}
+
+function getJShand(){
+  let js_hand_num = Math.floor(Math.random() * 3);
+  let hand;
+  if (js_hand_num == 0){
+    hand == "グー";
+  } else if (js_hand_num == 1){
+    hand == "チョキ";
+  } else if (js_hand_num == 2){
+    hand == "パー";
+  }
+  return hand;
+}
+
+function winLose(user, js){
+  
 }
